865b3e837b761373f848b92e40142456fc12eb25
Dmitriy <36476206+Onemoretitle@users.noreply.github.com>
2020-04-16 13:50:42 +0700

    на всякий случай. тут почти ничего

--- PersonnelDeptApp1/Form2.Designer.cs
+++ PersonnelDeptApp1/Form2.Designer.cs
@@ -36,11 +36,11 @@
             this.спарвкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.начинаяРаботуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.приказыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.приказыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -107,48 +107,6 @@
             this.начинаяРаботуToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
             this.начинаяРаботуToolStripMenuItem.Text = "Начиная работу";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(330, 329);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(175, 125);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Личная карточка";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(690, 329);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(175, 125);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Табель учёта рабочего времени";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(492, 687);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(225, 50);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Выход";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(317, 130);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(548, 73);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "ОТДЕЛ КАДРОВ";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
             // приказыToolStripMenuItem
             // 
             this.приказыToolStripMenuItem.Name = "приказыToolStripMenuItem";
@@ -156,12 +114,50 @@
             this.приказыToolStripMenuItem.Text = "Приказы";
             this.приказыToolStripMenuItem.Click += new System.EventHandler(this.приказыToolStripMenuItem_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(766, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 50);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Личная карточка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(972, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 50);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Табель учёта рабочего времени";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MistyRose;
+            this.button3.Location = new System.Drawing.Point(1082, 699);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 50);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Выход";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 101);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1160, 592);
+            this.panel1.TabIndex = 6;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1184, 761);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -183,12 +179,12 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ToolStripMenuItem сменитьПользователяToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem добавитьПользователяToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem удалитьПользователяToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem начинаяРаботуToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem приказыToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
     }
 }
