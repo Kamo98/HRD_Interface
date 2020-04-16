371088eb30b1806bbea86cdbd80efd29079fe833
Kamo98 <avanesjan.k@gmail.com>
2020-04-16 14:23:30 +0700

    start staff turnover

---
+++ PersonnelDeptApp1/StaffTurnover.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace PersonnelDeptApp1
+{
+	partial class StaffTurnover
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// StaffTurnover
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(847, 449);
+			this.Name = "StaffTurnover";
+			this.Text = "Отчётность";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
---
+++ PersonnelDeptApp1/StaffTurnover.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonnelDeptApp1
+{
+	public partial class StaffTurnover : Form
+	{
+		public StaffTurnover()
+		{
+			InitializeComponent();
+		}
+	}
+}
