8f02c85a0fb2a425861bc66950793f321a0aa9d9
Kamo98 <avanesjan.k@gmail.com>
2020-04-22 17:44:05 +0700

    Косяков нет

--- PersonnelDeptApp1/Form2.cs
+++ PersonnelDeptApp1/Form2.cs
@@ -23,6 +23,9 @@ namespace PersonnelDeptApp1
 
         private FormAuthorization formAuthorization;
 
+		BindingList<Department> departments = new BindingList<Department>();
+		BindingList<Occupation> occupations = new BindingList<Occupation>();
+
 		public Form2(FormAuthorization formAuthorization)
         {
 			InitializeComponent();
@@ -105,28 +108,63 @@ namespace PersonnelDeptApp1
 
         }
 
-        private void Form2_Load(object sender, EventArgs e)
+		private void FillDepts()
+		{
+			try
+			{
+				string sql = "select * from \"Unit\";";
+				if (Connection.get_connect() == null)
+					throw new NullReferenceException("Не удалось подключиться к базе данных");
+				NpgsqlCommand command = new NpgsqlCommand(sql, Connection.get_connect());
+				NpgsqlDataReader reader = command.ExecuteReader();
+				foreach (DbDataRecord record in reader)
+				{
+					Department newDept = new Department((int)record["pk_unit"], (string)record["Name"]);
+					departments.Add(newDept);
+
+				}
+				reader.Close();
+
+				comboBox1.DataSource = departments;
+				comboBox1.DisplayMember = "Name";
+				comboBox1.DataSource = departments;
+				comboBox1.AutoCompleteMode = AutoCompleteMode.Suggest;
+				comboBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+
+			}
+			catch (NullReferenceException ex)
+			{
+				MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Неизвестная ошибка.\n" + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+
+
+		private void Form2_Load(object sender, EventArgs e)
         {
-            AutoCompleteStringCollection listUnit_1 = new AutoCompleteStringCollection();
-            NpgsqlCommand com = new NpgsqlCommand("SELECT \"Name\" FROM \"Unit\"", npgSqlConnection);
-            NpgsqlDataReader reader = com.ExecuteReader();
+            //AutoCompleteStringCollection listUnit_1 = new AutoCompleteStringCollection();
+            //NpgsqlCommand com = new NpgsqlCommand("SELECT \"Name\" FROM \"Unit\"", npgSqlConnection);
+            //NpgsqlDataReader reader = com.ExecuteReader();
 
-            if (reader.HasRows)
-            {
-                foreach (DbDataRecord rec in reader)
-                {
-                    listUnit_1.Add(rec.GetString(0));
-                }
+            //if (reader.HasRows)
+            //{
+            //    foreach (DbDataRecord rec in reader)
+            //    {
+            //        listUnit_1.Add(rec.GetString(0));
+            //    }
 
-            }
-            reader.Close();
+            //}
+            //reader.Close();
 
-            comboBox1.DataSource = listUnit_1;
-            comboBox1.AutoCompleteMode = AutoCompleteMode.Suggest;
-            comboBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
-            comboBox1.AutoCompleteCustomSource = listUnit_1;
 
-        }
+			FillDepts();	
+
+
+		}
 
         private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
         {
@@ -138,32 +176,64 @@ namespace PersonnelDeptApp1
             
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+
+		private void FillOccups(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            AutoCompleteStringCollection listPos = new AutoCompleteStringCollection();
-            try
-            {
-                string sql = "select \"pos\".\"pk_position\", \"pos\".\"Name\", \"pos\".\"Rate\""
-                                + " from \"Position\" as \"pos\", \"Unit\" as \"un\""
-                                + " where \"pos\".\"pk_unit\" = \"un\".\"pk_unit\" and \"un\".\"pk_unit\" = " + ((sender as ComboBox).SelectedItem as Department).Id + ";";
-                NpgsqlCommand command = new NpgsqlCommand(sql, Connection.get_connect());
-                NpgsqlDataReader reader = command.ExecuteReader();
-                foreach (DbDataRecord record in reader)
-                {
-                    listPos.Add(record.GetString(0));
-                }
-                reader.Close();
+            //AutoCompleteStringCollection listPos = new AutoCompleteStringCollection();
+            //try
+            //{
+            //    string sql = "select \"pos\".\"pk_position\", \"pos\".\"Name\", \"pos\".\"Rate\""
+            //                    + " from \"Position\" as \"pos\", \"Unit\" as \"un\""
+            //                    + " where \"pos\".\"pk_unit\" = \"un\".\"pk_unit\" and \"un\".\"pk_unit\" = " + ((sender as ComboBox).SelectedItem as Department).Id + ";";
+            //    NpgsqlCommand command = new NpgsqlCommand(sql, Connection.get_connect());
+            //    NpgsqlDataReader reader = command.ExecuteReader();
+            //    foreach (DbDataRecord record in reader)
+            //    {
+            //        listPos.Add(record.GetString(0));
+            //    }
+            //    reader.Close();
 
-                comboBox2.DataSource = listPos;
-                comboBox2.AutoCompleteMode = AutoCompleteMode.Suggest;
-                comboBox2.AutoCompleteSource = AutoCompleteSource.CustomSource;
-                comboBox2.AutoCompleteCustomSource = listPos;
+            //    comboBox2.DataSource = listPos;
+            //    comboBox2.AutoCompleteMode = AutoCompleteMode.Suggest;
+            //    comboBox2.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            //    comboBox2.AutoCompleteCustomSource = listPos;
 
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Неизвестная ошибка.\n" + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show("Неизвестная ошибка.\n" + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
+
+
+			occupations.Clear();
+
+			try
+			{
+				string sql = "select \"pos\".\"pk_position\", \"pos\".\"Name\", \"pos\".\"Rate\""
+								+ " from \"Position\" as \"pos\", \"Unit\" as \"un\""
+								+ " where \"pos\".\"pk_unit\" = \"un\".\"pk_unit\" and \"un\".\"pk_unit\" = " + ((sender as ComboBox).SelectedItem as Department).Id + ";";
+				NpgsqlCommand command = new NpgsqlCommand(sql, Connection.get_connect());
+				NpgsqlDataReader reader = command.ExecuteReader();
+				foreach (DbDataRecord record in reader)
+				{
+					occupations.Add(new Occupation((int)record["pk_position"], (string)record["Name"], (decimal)record["Rate"]));
+				}
+				reader.Close();
+
+				comboBox2.DataSource = occupations;
+				comboBox2.DisplayMember = "Name";
+
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Неизвестная ошибка.\n" + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+
+		}
+	}
 }
